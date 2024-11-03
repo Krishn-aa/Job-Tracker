d0defaff0f89d7158b44e609c7cb86d393ee098a
Krishna-aa <krishnaanand0001@gmail.com>
2024-11-03 22:05:28 +0530

    added config for frontend deployment

--- client/job-tracker/src/services/api.js
+++ client/job-tracker/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 //Companies
 export const getCompanies = () => axios.get(`${API_BASE_URL}/companies`);
