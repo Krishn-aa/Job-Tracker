87bedc36308d29d5f586fffd85f386baf7fd3360
Krishna-aa <krishnaanand0001@gmail.com>
2024-11-03 21:28:00 +0530

    added fevicon and mongo cloud

--- client/job-tracker/src/pages/Home.jsx
+++ client/job-tracker/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
 
   return (
     <div className="p-4">
-      <h1 className="text-3xl font-bold mb-2">Job Application Tracker</h1>
+      <h1 className="text-3xl font-bold mb-2">Job Tracker</h1>
       <p className="text-lg text-gray-600 mb-6">{todayDate}</p>
       <AddCompany onAdd={() => window.location.reload()} />
       <CompanyList />
